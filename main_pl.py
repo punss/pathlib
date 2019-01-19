980a5f40386d5724122bf47199e9280674852a92
Hridik Punukollu <45720230+punss@users.noreply.github.com>
2019-01-19 22:48:18 +0530

    Update main_pl.py

--- main_pl.py
+++ main_pl.py
@@ -6,52 +6,59 @@ for (url in urls) {
 }"""
 
 
-
 from bs4 import BeautifulSoup
 import requests
 import os
 from pathlib import Path
 
-z=Path()
-z=z.resolve()
+z = Path()
+z = z.resolve()
+Z_STRING = str(z)
+
+
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def findtxt(word):
 
-    v=Path(str(z)).joinpath("wordlists")
+def find_text(word):
+
+    v = Path(Z_STRING).joinpath("wordlists")
     for i in v.iterdir():
-        tempvar=Path(str(v.resolve())).joinpath(str(i))
-        #print(tempvar)
-        q=Path(tempvar)
-        r=Path(str(z)).joinpath("lis.txt")
-        t=r.open("a")
+        path_of_file = Path(str(v.resolve())).joinpath(str(i))
+        q = Path(path_of_file)
+        r = Path(Z_STRING).joinpath("lis.txt")
+        t = r.open("a")
         with q.open("r", encoding="ascii", errors='ignore') as u:
-            worl=u.readlines()
-            worl=list(map(str.rstrip, worl))
-        if word in worl:
+            WORD_LIST = u.readlines()
+            WORD_LIST = list(map(str.rstrip, WORD_LIST))
+        if word in WORD_LIST:
             print(str(word)+" found")
             t.write(word+': ')
             t.write(str(i)[len(str(v))+1:str(i).index('_wordlist')])
             t.write("\n")
             return
     else:
-        print(str(word)+ " not found")
-        t.write(str(word)+ " not found\n")
+        print(str(word) + " not found")
+        t.write(str(word) + " not found\n")
         u.close()
 
-def wrdlist(x):
-    url=("http://video.google.com/timedtext?lang=en&v=%s" % (x))
-    headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
-    response=requests.get(url,headers=headers)
-    soup=BeautifulSoup(response.text,'lxml')
 
-    g=Path(str(z)).joinpath("IDs",("ID_%s.txt" % x))
-    f=g.open("w+")
+def word_list(x):
+    url = ("http://video.google.com/timedtext?lang=en&v=%s" % (x))
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110'
+        'Safari/537.36'
+        }
+    response = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    g = Path(Z_STRING).joinpath("IDs", ("ID_%s.txt" % x))
+    f = g.open("w+")
     f.write(soup.get_text())
     f.close()
-    l=[]
-    m=[]
+    l = []
+    m = []
     with g.open("r") as f:
         for words in f:
             for word in words.split():
@@ -59,56 +66,58 @@ def wrdlist(x):
                     l.append(word.lower())
 
         for i in l:
-            i=i.replace("&#39;","'")
-            i=i.replace("&quot;","")
-            i=i.replace("&gt;","")
-            i=i.replace(",","")
-            i=i.replace(".","\n")
-            i=i.replace("?","")
-            i=i.replace("!","")
+            i = i.replace("&#39;", "'")
+            i = i.replace("&quot;", "")
+            i = i.replace("&gt;", "")
+            i = i.replace(",", "")
+            i = i.replace(".", "\n")
+            i = i.replace("?", "")
+            i = i.replace("!", "")
             m.append(i)
-        
-        n=Path(str(z)+"/wordlists/ID:%s_wordlist.txt" % x)
-        f=n.open("w+")
+        n = Path(Z_STRING+"/wordlists/ID:%s_wordlist.txt" % x)
+        f = n.open("w+")
         for i in m:
             f.write(i)
             f.write("\n")
         f.close()
 
-g=int(input("How many videos do you want to convert to word lists?: "))
-j=Path(str(z)).joinpath("pewds.txt")
-d=Path(str(z)).joinpath("lis.txt")
-f=j.open("r")
-c=0
-t=d.open("w")
+
+NUM_OF_VID = int(input(
+    "How many videos do you want to convert to word lists?: "
+    ))
+j = Path(Z_STRING).joinpath("pewds.txt")
+d = Path(Z_STRING).joinpath("lis.txt")
+f = j.open("r")
+COUNT = 0
+t = d.open("w")
 t.close()
-if not Path.exists(Path(str(z)).joinpath("wordlists")):
-    y=Path(str(z)).joinpath("wordlists")
+if not Path.exists(Path(Z_STRING).joinpath("wordlists")):
+    y = Path(Z_STRING).joinpath("wordlists")
     y.mkdir()
-    #"wordlists/"
 else:
     pass
-if not Path.exists(Path(str(z)).joinpath("IDs")):
-    x=Path(str(z)).joinpath("IDs")
+if not Path.exists(Path(Z_STRING).joinpath("IDs")):
+    x = Path(Z_STRING).joinpath("IDs")
     x.mkdir()
-    #'/IDs'
 else:
     pass
 
-while (c!=g):
-    temp_=f.readline()
-    wrdlist(temp_.rstrip())
-    c+=1
+while (COUNT != NUM_OF_VID):
+    word_in_line = f.readline()
+    word_list(word_in_line.rstrip())
+    COUNT += 1
     cls()
-    print(str(c)+" of " + str(g) +" videos converted into word lists.")
+    print(
+        str(COUNT) + " of " + str(NUM_OF_VID) +
+        " videos converted into word lists.")
 cls()
-print(str(c)+" videos converted into word lists.")
+print(str(COUNT) + " videos converted into word lists.")
 
-ans=input("Do you want to look for the files that match the lyrics now?: ")
+ans = input("Do you want to look for the files that match the lyrics now?: ")
 
-if (ans=="y" or ans=="Y"):
-    lyr=list(map(str,input("Enter the text: ").split(" ")))
+if (ans == "y" or ans == "Y"):
+    lyr = list(map(str, input("Enter the text: ").split(" ")))
     for word in lyr:
-        findtxt(word)
+        find_text(word)
 else:
     exit(0)
